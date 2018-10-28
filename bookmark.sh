45cbc00681a484611681e4e1adaab65dbe4f7944
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2018-10-28 13:51:59 +0000

    bookmark works in ksh

---
+++ bookmark.sh
@@ -0,0 +1,30 @@
+
+function bookmark {
+	typeset OPTIND
+	typeset OPTARG
+	typeset o
+	typeset label
+	typeset dir
+	typeset print
+	while getopts ":l:d:p" o; do
+		case "${o}" in
+			l)
+				label="${OPTARG}"
+				;;
+			d)
+				dir="${OPTARG}"
+				;;
+			p)
+				print=1
+				;;
+		esac
+	done
+
+	if [ "$print" = 1 ]; then
+		echo $dir
+	elif [ "$label" ]; then
+		alias "$label"="bookmark -d $dir"
+	else
+		cd $dir
+	fi
+}
