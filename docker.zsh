8f9ccf33c310cc3859dd08d3a0d2452e03c0e979
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2020-04-19 16:07:38 +0100

    docker

--- docker.zsh
+++ docker.zsh
@@ -2,3 +2,5 @@
 #export PATH=/home/zoltan/bin:$PATH
 export DOCKER_HOST=unix:///run/user/1000/docker.sock
 
+alias docker_start='systemctl --user start docker'
+alias docker_stop='systemctl --user stop docker'
